22991609b427b33809ce96c53c2f1afb74cc6703
Vai1020 <102492742+Vai1020@users.noreply.github.com>
2023-04-22 22:14:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 public class hello
 {
 	public static void main(String[] args) {
-          int x =5 //first arguments.
-          int y = 10 //second arguments.
+          int x =5; //first arguments.
+          int y = 10; //second arguments.
           int sum = x + y;
           System.out.println("The sum of x and y is: " +sum);
 	}
